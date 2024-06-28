b6417451881ce80b02f95eaf094237150f8e3a1d
Dariana <dariana.gomez@uc.cl>
2024-06-28 12:46:46 -0400

    se agrega apiToken de Demo nueva

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -4,5 +4,5 @@ export const environment = {
     apiLibros: 'http://146.155.28.129:80/api/libros',
     apiPreguntasFrecuentes: 'http://146.155.28.129:80/api/preguntas-frecuentes',
     apiMaterialesEducativos: 'http://146.155.28.129:80/api/materiales-educativos',
-    apiToken: 'f8713e39ef8d6bf6edc3ccd5c14e1db9a0fecd4ef0caa30de661aaf677179ec4bbbc1421df6215bc92f63f7bea5835f01fd0c76ba9aadd2d8eb1db31654d6b7c4cf5458101e51bf3798ff561765819eb264cf631a42c41905c5eca11f3a0bc9899452e13ba4a6096a64080593865e470c0170174276c0c8b78376dfd0eb66806'
+    apiToken: 'ae89d9fdc27765b90e32cedb0abea59cdd493421dc5b0cc8a1d856c9f76e889bebffd231eb3617e35263afd4ceac1cf0ecd9df7b5b0224779237747b02d9541d8e812c7e4864212c8757ff2182646f37cd5ba8df095a87f71494ca67c8f9c9336e8c13f29868f786391d6de695f0d61537016c29dd9128155c6d87890397011b'
 };
