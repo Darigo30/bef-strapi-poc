9f98d3f076d1b81238191314ce28b7e0f7b4589f
Dariana <dariana.gomez@uc.cl>
2024-06-28 14:22:10 -0400

    se modifica apis demo env

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
     urlBase: 'http://146.155.28.129:80',
-    apiLibros: 'http://146.155.28.129:80/api/libros',
-    apiPreguntasFrecuentes: 'http://146.155.28.129:80/api/preguntas-frecuentes',
-    apiMaterialesEducativos: 'http://146.155.28.129:80/api/materiales-educativos',
+    apiLibros: 'http://146.155.28.129:80/api/libros?populate=*',
+    apiPreguntasFrecuentes: 'http://146.155.28.129:80/api/preguntas-frecuentes?populate=*',
+    apiMaterialesEducativos: 'http://146.155.28.129:80/api/materiales-educativos?populate=*',
     apiToken: 'ae89d9fdc27765b90e32cedb0abea59cdd493421dc5b0cc8a1d856c9f76e889bebffd231eb3617e35263afd4ceac1cf0ecd9df7b5b0224779237747b02d9541d8e812c7e4864212c8757ff2182646f37cd5ba8df095a87f71494ca67c8f9c9336e8c13f29868f786391d6de695f0d61537016c29dd9128155c6d87890397011b'
 };
